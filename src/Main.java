00aefd0e1c147e4b03b724bbe970661a129cf489
Ta0huu <Anuwatch6552@gmail.com>
2023-12-16 20:11:15 +0700

    anuwatch

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,48 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Set;
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        Set<String> f = new HashSet<>();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        f.add("yee");
+        f.add("eiei");
+        f.add("team");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("------------------------------------------------");
+        System.out.println("Set Hash: " + f);
+        System.out.println("Size: " + f.size());
+        System.out.println("empty: " + f.isEmpty());
+        System.out.println("contain 'yee' :" + f.contains("yee"));
+        System.out.println("HashSet f: ");
+        for (String u : f) {
+            System.out.println(u);
         }
+        System.out.println("------------------------------------------------");
+        Object[] ar = f.toArray();
+        System.out.println("HashSet: ");
+        for (Object obj : ar) {
+            System.out.println(obj);
+        }
+        f.remove("eiei");
+
+
+
+        System.out.println("removing 'eiei': " + f);
+        System.out.println("Size: " + f.size());
+        System.out.println("empty: " + f.isEmpty());
+
+        f.clear();
+
+        System.out.println("after clear f: " + f);
+        System.out.println("Size: " + f.size());
+        System.out.println("empty: " + f.isEmpty());
+
+    }
+    public void display(Set<String> Hash){
+        System.out.println("------------------------------------------------");
+        System.out.println("Set Hash : " + Hash);
+
+        System.out.println("------------------------------------------------");
     }
 }
